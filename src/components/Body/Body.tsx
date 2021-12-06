dec2176ab9c487847e23e7d41796893801591d94
tishka25 <teodorstanishev@gmail.com>
2021-12-06 22:38:44 +0200

    add social links

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ScrollFor from '../ScrollFor/ScrollFor';
 import SideMenu from '../SideMenu/SideMenu';
+import SideSocialLinks from '../SideSocialLinks/SideSocialLinks';
 import './style.css'
 
 export default function Body() {
@@ -9,7 +10,7 @@ export default function Body() {
       <div className="mainBody-container">
         <SideMenu />
         <ScrollFor className="mainBody-arrow-down-container" title="Scroll for Lolita News" href="#"/>
-        <div style={{ backgroundColor:'blue' }}>asd</div>
+        <SideSocialLinks />
 
       </div>
     </div>
---
+++ src/components/SideSocialLinks/SideSocialLinks.tsx
@@ -0,0 +1,22 @@
+import React, { MouseEvent, MouseEventHandler } from 'react'
+import Icon from '../Icon/Icon'
+import { getClassName, Styleable } from '../utils/utils'
+import './style.css'
+
+export interface SideSocialLinksProps extends Styleable {
+}
+export default function SideSocialLinks(props: SideSocialLinksProps) {
+  function renderSideMenuItem(icon: string) {
+    return (
+        <Icon src={icon}/>
+    );
+  }
+  return (
+    <div className={getClassName("sideSocialLinks", props)} style={props.style}>
+      {renderSideMenuItem(require('../../assets/icons/facebook logo.png').default)}
+      {renderSideMenuItem(require('../../assets/icons/youtube logo.svg').default)}
+      {renderSideMenuItem(require('../../assets/icons/instagram logo.svg').default)}
+      {renderSideMenuItem(require('../../assets/icons/linkedin logo.png').default)}
+    </div>
+  )
+}
