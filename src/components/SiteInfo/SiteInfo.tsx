7500578bc5a2372c8db1f806ce523da2eda77796
tishka25 <teodorstanishev@gmail.com>
2021-12-08 12:39:16 +0200

    site info retrieve data; update site info on component mount

--- src/components/SiteInfo/SiteInfo.tsx
+++ src/components/SiteInfo/SiteInfo.tsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SiteInfoRow from '../SiteInfoRow/SiteInfoRow'
-import { getClassName, Styleable } from '../utils/utils'
+import { SITE_INFO_API } from '../utils/constants';
+import { getClassName, getFetchData, Styleable } from '../utils/utils'
 import './style.css'
 
+const defaultSiteInfo = {
+  prefix: 'Over',
+  count: '1/2',
+  suffix: 'tree saved',
+  trees: 0.6,
+};
 export interface SiteInfoProps extends Styleable{
 }
 export default function SiteInfo(props: SiteInfoProps) {
+  const [siteInfo, setSiteInfo] = useState(defaultSiteInfo);
+  useEffect(() => {
+    const loadData = async () => {
+      const response = await getFetchData(SITE_INFO_API);
+      setSiteInfo(response);
+    }
+    loadData();
+  }, [])
   return (
     <div className={getClassName('siteInfo', props)} style={props.style}>
       <div className="siteInfo-title">Lolita Mercedes</div>
       <div className="siteInfo-container">
-        <SiteInfoRow text=" ads blocked" value="10" valueFirst valueSize={30} textSize={20}/>
-        <SiteInfoRow text="Trees saved " value="0.57" valueSize={30} textSize={20}/>
+        <SiteInfoRow value="10" text=" ads blocked" valueFirst valueSize={30} textSize={20}/>
+        <SiteInfoRow text={siteInfo.prefix} value={siteInfo.count} suffix={siteInfo.suffix} margin={4} valueSize={30} textSize={20}/>
       </div>
     </div>
   )
