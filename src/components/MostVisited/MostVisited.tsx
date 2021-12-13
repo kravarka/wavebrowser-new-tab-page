8c3be221f7b7ec22e2a0f216f88e6b85df87ddf5
tishka25 <teodorstanishev@gmail.com>
2021-12-13 20:46:01 +0200

    implement basic embeddedSearch most visited sites

--- src/components/MostVisited/MostVisited.tsx
+++ src/components/MostVisited/MostVisited.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Icon from '../Icon/Icon';
 import './style.css'
 
+export interface MostVisitedItem {
+  rid: number;
+  faviconUrl: string;
+}
 export default function MostVisited() {
+  const [mostVisitedSites, setMostVisitedSites] = useState<MostVisitedItem[]>([]);
+  // Load most visited sites
+  useEffect(() => {
+    if((window as any).chrome instanceof Object) {
+      if((window as any).chrome.embeddedSearch instanceof Object) {
+        const mostVisited = (window as any).chrome.embeddedSearch.mostVisited as MostVisitedItem[];
+        setMostVisitedSites(mostVisited);
+      }
+    }
+  }, []);
+
+  function renderItem(url: string, iconUrl: string) {
+    return (
+      <div className="mostVisitedItem">
+        <Icon src={iconUrl} className="mostVisitedItemIcon"/>
+        <iframe src={url} />
+      </div>
+    );
+  }
+
+  function renderMostVisitedSites() {
+    return mostVisitedSites.map(item=>{
+      const url = `chrome-search://most-visited/title.html?rid=${item.rid}`;
+      return renderItem(url, item.faviconUrl);
+    });
+  }
+
   return (
     <div className="mostVisited">
-      <iframe src="chrome://new-tab-page-third-party" />
+      {renderMostVisitedSites()}
     </div>
   )
 }
