8d6b923edcd5516f5fbe88fb1552cf3548f3f995
tishka25 <teodorstanishev@gmail.com>
2021-12-13 09:59:17 +0200

    add basic implementation for most visited sites

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import MostVisited from '../MostVisited/MostVisited';
 import SideMenu from '../SideMenu/SideMenu';
 import SideSocialLinks from '../SideSocialLinks/SideSocialLinks';
 import './style.css'
@@ -9,6 +10,7 @@ export default function Body() {
       <div className="mainBody-container" id="main">
         <SideMenu className="mainBody-sideMenu"/>
         <SideSocialLinks className="mainBody-socialLinks" />
+        <MostVisited />
       </div>
     </div>
   )
---
+++ src/components/MostVisited/MostVisited.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css'
+
+export default function MostVisited() {
+  return (
+    <div className="mostVisited">
+      <div>asddsa</div>
+      <div>asddsa</div>
+      <div>asddsa</div>
+      <div>asddsa</div>
+
+    </div>
+  )
+}
