343b0699efc2192e25ff6fffbc7fe39792931bcb
tishka25 <teodorstanishev@gmail.com>
2021-12-27 14:33:16 +0200

    news

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import MainPage from './components/MainPage/MainPage';
-import { getParameterByName, handleSearch } from './components/utils/utils';
 
 function App() {
-  const [searchQuery, setSearchQuery] = useState('');
-  useEffect(() => {
-    const onChange = () => {
-      setSearchQuery(getParameterByName('q') || '');
-    }
-    onChange();
-    window.addEventListener('hashchange', onChange);
-    return () => {
-      window.removeEventListener('hashchange', onChange);
-    }
-  }, []);
-
-  useEffect(()=>{
-    handleSearch(searchQuery);
-  }, [searchQuery]);
-
   return (
     <MainPage/>
   );
--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import ScrollFor from '../ScrollFor/ScrollFor'
 import { getRandomArbitrary, map, Styleable } from '../utils/utils'
 import './style.css'
 //@ts-ignore
@@ -61,8 +60,6 @@ export interface NewsProps extends Styleable {
 }
 export default function News(props: NewsProps) {
   const [items, setItems] = useState(defaultItems);
-  const [waypointOffset, setWaypointOffset] = useState({ offsetBottom: 0, height: 0, offsetTop: 0 });
-  const [arrowDownOpacity, setArrowDownOpacity] = useState(1);
 
   useEffect(() => {
     const newItems: { title: string, image: string }[] = [];
@@ -80,37 +77,6 @@ export default function News(props: NewsProps) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // Get waypoint offset
-  useEffect(() => {
-    const arrowDown = document.querySelector(".news-arrow-down-container");
-    if (arrowDown) {
-      const rect = arrowDown.getBoundingClientRect();
-      const waypoint = {
-        offsetBottom: window.innerHeight - rect.bottom,
-        offsetTop: window.innerHeight - rect.top,
-        height: rect.height,
-      }
-      setWaypointOffset(waypoint);
-    }
-  }, []);
-
-  // Register scroll event
-  useEffect(() => {
-    const onScroll = () => {
-      const scrollPosition = window.scrollY;
-      const arrowDown = document.querySelector(".news-arrow-down-container");
-      if (arrowDown) {
-        var opacity = map(scrollPosition, waypointOffset.offsetBottom, waypointOffset.offsetBottom + waypointOffset.height, 1, 0);
-        opacity = Math.max(0, opacity);
-        opacity = Math.min(1, opacity);
-          setArrowDownOpacity(opacity);
-      }
-    };
-    document.addEventListener('scroll', onScroll, { passive: true });
-    return () => {
-      document.removeEventListener('scroll', onScroll);
-    }
-  }, [waypointOffset])
 
   function renderMasonryGridItemButtons() {
     return (
@@ -136,17 +102,14 @@ export default function News(props: NewsProps) {
     })
   }
   return (
-    <div id="news" className="news">
-      <ScrollFor className="news-arrow-down-container" style={{ opacity: arrowDownOpacity }} title="Scroll for Lolita News" />
-      <div className="news-container">
-        <span />
-        <div className="news-content">
-          <Masonry columnsCount={2} gutter="20px">
-            {renderMasonryGridItem()}
-          </Masonry>
-        </div>
-        <span />
+    <div className="news-container">
+      <span />
+      <div className="news-content">
+        <Masonry columnsCount={2} gutter="20px">
+          {renderMasonryGridItem()}
+        </Masonry>
       </div>
+      <span />
     </div>
   )
 }
