2ed27357857360cea0750a4568749e252c8eee47
tishka25 <teodorstanishev@gmail.com>
2021-12-27 16:05:14 +0200

    add postmessage so that the parent can know the iframe child height

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -3,6 +3,14 @@ import News from '../News/News';
 import './style.css'
 
 export default function MainPage() {
+  useEffect(() => {
+  }, [])
+  useEffect(() => {
+    // Send height to parent
+    window.onload = ()=>{
+      window.parent.postMessage(document.body.clientHeight, '*');
+    };
+  }, [])
   return (
     <News />
   )
